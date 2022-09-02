abe0f437c4b6bf9c60fd4f2e52b0a006fea3a60c
CamiloAvelar <camilo.avelar@gmail.com>
2022-09-02 19:10:07 +0000

    feat: add maxNumberOfMessages param for the MessageAdapters

--- src/messageAdapters/MessageAdapter.ts
+++ src/messageAdapters/MessageAdapter.ts
@@ -8,7 +8,12 @@ export interface Message {
     delay: (seconds: number) => Promise<void>
 }
 
+export type MessageAdapterParams = {
+    maxNumberOfMessages: number
+};
+
 export abstract class MessageAdapter {
+    public abstract maxNumberOfMessages: number;
     public abstract receive(maxMessages?: number): Promise<Message[]>;
     public abstract delete(id: string): Promise<void>;
     public abstract delay(id: string, seconds: number): Promise<void>;
--- src/messageAdapters/SQSAdapter.ts
+++ src/messageAdapters/SQSAdapter.ts
@@ -1,59 +1,65 @@
-import { SQS, } from 'aws-sdk';
-import { MessageAdapter, Message } from './MessageAdapter';
+import { SQS } from 'aws-sdk';
+import { MessageAdapter, Message, MessageAdapterParams } from './MessageAdapter';
 
-export interface SQSAdapterParams {
+export interface SQSAdapterParams extends MessageAdapterParams {
     queueURL: string
     sqs?: SQS.Types.ClientConfiguration
 }
 
 export class SQSAdapter implements MessageAdapter {
-    private _sqs: SQS;
-    private _queueURL: string;
+  public maxNumberOfMessages: number;
+  private _sqs: SQS;
+  private _queueURL: string;
 
-    constructor(params: SQSAdapterParams) {
-        this._queueURL = params.queueURL;
-        this._sqs = new SQS({
-            apiVersion: '2012-11-05',
-            ...(params.sqs ? params.sqs : undefined)
-        });
+  constructor(params: SQSAdapterParams) {
+    this.maxNumberOfMessages = params.maxNumberOfMessages;
+    this._queueURL = params.queueURL;
+    this._sqs = new SQS({
+      apiVersion: '2012-11-05',
+      ...(params.sqs ? params.sqs : undefined),
+    });
+  }
+
+  public async receive(maxMessages?: number): Promise<Message[]> {
+    const response = await this._sqs.receiveMessage({
+      MaxNumberOfMessages: maxMessages || 10,
+      QueueUrl: this._queueURL,
+      VisibilityTimeout: 30,
+      WaitTimeSeconds: 20,
+    }).promise();
+
+    if(!response.Messages) {
+      return [];
     }
 
-    public async receive(maxMessages?: number): Promise<Message[]> {
-        const response = await this._sqs.receiveMessage({
-            MaxNumberOfMessages: maxMessages || 10,
-            QueueUrl: this._queueURL,
-            VisibilityTimeout: 30,
-            WaitTimeSeconds: 20
-        }).promise();
+    return response.Messages.map((message: SQS.Message) => {
+      return {
+        id: message.MessageId,
+        receipt: message.ReceiptHandle,
+        payload: message.Body,
+        receivedTimestamp: Date.now(),
+        delete: async () => {
+          return this.delete(message.ReceiptHandle as string);
+        },
+        delay: async (seconds: number) => {
+          return this.delay(message.ReceiptHandle as string, seconds);
+        },
+      } as Message;
+    });
+  }
 
-        if(!response.Messages) {
-            return [];
-        }
+  public async delete(id: string): Promise<void> {
+    await this._sqs.deleteMessage({
+      QueueUrl: this._queueURL,
+      ReceiptHandle: id,
+    }).promise();
+  }
 
-        return response.Messages?.map((message: SQS.Message) => {
-            return {
-                id: message.MessageId,
-                receipt: message.ReceiptHandle,
-                payload: message.Body,
-                receivedTimestamp: Date.now(),
-                delete: async () => { return this.delete(message.ReceiptHandle as string) },
-                delay: async (seconds: number) => { return this.delay(message.ReceiptHandle as string, seconds) }
-            } as Message
-        })
-    }
-
-    public async delete(id: string): Promise<void> {
-        await this._sqs.deleteMessage({
-            QueueUrl: this._queueURL,
-            ReceiptHandle: id
-        }).promise();
-    }
-
-    public async delay(id: string, seconds: number): Promise<void> {
-        await this._sqs.changeMessageVisibility({
-            QueueUrl: this._queueURL,
-            ReceiptHandle: id,
-            VisibilityTimeout: seconds
-        }).promise();
-    }
+  public async delay(id: string, seconds: number): Promise<void> {
+    await this._sqs.changeMessageVisibility({
+      QueueUrl: this._queueURL,
+      ReceiptHandle: id,
+      VisibilityTimeout: seconds,
+    }).promise();
+  }
 }
